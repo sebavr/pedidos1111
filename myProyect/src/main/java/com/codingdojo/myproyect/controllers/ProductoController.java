3826152bbc9b35f7bd43301d5f9cd1c664c655bd
alvllnkvl <alvllnkvl@gmail.com>
2021-08-10 23:17:43 -0400

    detallesProducto

--- myProyect/src/main/java/com/codingdojo/myproyect/controllers/ProductoController.java
+++ myProyect/src/main/java/com/codingdojo/myproyect/controllers/ProductoController.java
@@ -3,9 +3,12 @@ package com.codingdojo.myproyect.controllers;
 import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.codingdojo.myproyect.models.Producto;
@@ -31,5 +34,13 @@ public class ProductoController {
 	
 		return "redirect:/admin/dashboard";
 	}
-	
+	@RequestMapping("/producto/{id}")
+	public String detallesProducto(@PathVariable("id") Long id,Model model) {
+		Producto producto= productoService.findProducto(id);
+		if(producto==null) {
+			return "redirect:/pedir";
+		}
+		model.addAttribute("producto", producto);
+		return "detallesProducto.jsp";
+	}
 }
